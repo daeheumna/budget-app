eba9a00a7a2f5deac2f0e85785b8675aaf757b0c
Dae Heum Na <daeheumna@gmail.com>
2023-06-26 17:22:22 -0400

    fixed an issue where user was directed to error page upon creating an account

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -1,19 +1,18 @@
+// rrd imports
 import { redirect } from "react-router-dom";
-
-// helpers
-import { deleteItem  } from "../helpers";
 
 // library
 import { toast } from "react-toastify";
 
-export async function logoutAction(){
-    // delete the user
-    deleteItem({
-        key: 'userName'
-    })
+// helpers
+import { deleteItem } from "../helpers";
 
-    toast.success("You've deleted your account!")
-
-    // return redirect
-    return redirect('/')
+export async function logoutAction() {
+  // delete the user
+  deleteItem({
+    key: "userName"
+  })
+  toast.success("You’ve deleted your account!")
+  // return redirect
+  return redirect("/")
 }
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -2,32 +2,31 @@
 import { Outlet, useLoaderData } from "react-router-dom";
 
 // assets
-import wave from "../assets/wave.svg"
+import wave from "../assets/wave.svg";
 
 // components
 import Nav from "../components/Nav";
 
-// helper functions
+//  helper functions
 import { fetchData } from "../helpers"
 
 // loader
-export function mainLoader(){
-    const userName = fetchData('userName');
-    return { userName }
+export function mainLoader() {
+  const userName = fetchData("userName");
+  return { userName }
 }
 
 const Main = () => {
-    const { userName } = useLoaderData();
+  const { userName } = useLoaderData()
 
-    return (
-        <div className='layout'>
-            <Nav userName={userName}/>
-            <main>
-                <Outlet />
-            </main>
-            <img src={wave} alt=''/>
-        </div>
-    )
+  return (
+    <div className="layout">
+      <Nav userName={userName} />
+      <main>
+        <Outlet />
+      </main>
+      <img src={wave} alt="" />
+    </div>
+  )
 }
-
 export default Main
