557afc6c594153134974379b3a63439fbc8a5b6f
Dae Heum Na <daeheumna@gmail.com>
2023-06-28 15:59:29 -0400

    added budget cards that show the created budgets & expenses

--- src/helpers.js
+++ src/helpers.js
@@ -48,3 +48,36 @@ export const createExpense = ({
 export const deleteItem = ({ key }) => {
   return localStorage.removeItem(key)
 }
+
+// Total spent by budget
+export const calculateSpentByBudget = (budgetId) => {
+    const expenses = fetchData('expenses') ?? [];
+    const budgetSpent = expenses.reduce((acc, expense) => {
+        // check if expense.id === budgetId that was passed
+        if(expense.budgetId !== budgetId) return acc
+
+        // add the current amount to my total
+        return acc += expense.amount
+    }, 0)
+    return budgetSpent;
+}
+
+
+// Formating
+
+
+// Formating percentages
+export const formatPercentage = (amt) => {
+    return amt.toLocaleString(undefined, {
+        style: 'percent',
+        minimumFractionDigits: 0
+    })
+}
+
+// Format currency
+export const formatCurrency = (amt) => {
+    return amt.toLocaleString(undefined, {
+        style: "currency",
+        currency: "USD"
+    })
+}
